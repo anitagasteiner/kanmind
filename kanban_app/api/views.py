8aa5bc8a49573e0fc88f3693d14bc3e91487366e
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-10 17:19:48 +0100

    Tasks View added

---
+++ kanban_app/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from models import Task
+from serializers import TaskSerializer
+
+class TasksView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    
