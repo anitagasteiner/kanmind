3636554365f3ee8de387bd38d623e218eddc7073
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-10 17:01:22 +0100

    Task model created

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):    
+    TO_DO = "to_do"
+    IN_PROGRESS = "in_progress"
+    REVIEW = "review"
+    DONE = "done"
+    STATUS_CHOICES = {
+        TO_DO: "to_do",
+        IN_PROGRESS: "in_progress",
+        REVIEW: "review",
+        DONE: "done"
+    }
+
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+    PRIORITY_CHOICES = {
+        LOW: "low",
+        MEDIUM: "medium",
+        HIGH: "high"
+    }
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default=TO_DO)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default=MEDIUM)
+    due_date = models.DateField()
+
+    
