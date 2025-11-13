a07a1864c6dbf3a116249dfe5904df359c9286b1
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-13 09:45:40 +0100

    logout enabled

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -5,7 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
-    #path('login/', CustomLoginView.as_view(), name='login'),
     path('login/', obtain_auth_token, name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -2,8 +2,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-#from rest_framework.authtoken.models import Token
-#from rest_framework.authtoken.views import ObtainAuthToken
 from .serializers import UserSerializer
 
 
@@ -25,24 +23,5 @@ class LogoutView(APIView):
     def post(self, request):
         request.user.auth_token.delete()
         return Response({"detail": "Logout successful. Token deleted."}, status=status.HTTP_200_OK)
+
     
-# class CustomLoginView(ObtainAuthToken):
-#     permission_classes = [AllowAny]
-
-#     def post(self, request):
-#         serializer = self.serializer_class(data=request.data)
-#         data={}
-
-#         if serializer.is_valid():
-#             user = serializer.validated_data['user']
-#             token, created = Token.objects.get_or_create(user=user)
-#             data = {
-#                 'token': token.key,
-#                 'username': user.username,
-#                 'email': user.email
-#             }
-
-#         else:
-#             data = serializer.errors
-
-#         return Response(data)
