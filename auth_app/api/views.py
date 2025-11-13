dc9c96a111b050457f19a16a5edc2d246f0e0192
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-13 11:08:09 +0100

    registration response optimized

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -14,8 +14,15 @@ class RegistrationView(APIView):
         serializer = UserSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            fullname = f"{user.first_name} {user.last_name}".strip()
+            return Response({
+                'token': token.key,
+                'fullname': fullname,
+                'email': user.email,
+                'user_id': user.id                
+                }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
@@ -53,6 +60,6 @@ class LogoutView(APIView):
 
     def post(self, request):
         request.user.auth_token.delete()
-        return Response({"detail": "Logout successful. Token deleted."}, status=status.HTTP_200_OK)
+        return Response({'detail': 'Logout successful. Token deleted.'}, status=status.HTTP_200_OK)
 
     
