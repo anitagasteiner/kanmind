9f11a561dae915c21f1e9b9ecaca2b3dac4661f7
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-14 14:15:52 +0100

    owner_id and member_count added to board

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -26,6 +26,8 @@ class BoardSerializer(serializers.ModelSerializer):
     # )
     # ticket_count = serializers.SerializerMethodField()
 
+    member_count = serializers.SerializerMethodField()
+
     tasks = TaskSerializer(many=True, read_only=True)
     task_ids = serializers.PrimaryKeyRelatedField(
         queryset=Task.objects.all(),
@@ -36,11 +38,15 @@ class BoardSerializer(serializers.ModelSerializer):
     ticket_count = serializers.SerializerMethodField()
     tasks_to_do_count = serializers.SerializerMethodField()
     tasks_high_prio_count = serializers.SerializerMethodField()
+    owner_id = serializers.IntegerField(source='owner.id', read_only=True)
     
     class Meta:
         model = Board
-        fields = ['id', 'title', 'tasks', 'task_ids', 'ticket_count', 'tasks_to_do_count','tasks_high_prio_count']
+        fields = ['id', 'title', 'member_count', 'tasks', 'task_ids', 'ticket_count', 'tasks_to_do_count','tasks_high_prio_count', 'owner_id']
         read_only_fields = ['id']
+
+    def get_member_count(self, obj):
+        return obj.members.count() #TODO - Woher kommt "members"? Das gibt's doch gar nicht, oder?
 
     def get_ticket_count(self, obj):
         return obj.tasks.count()
