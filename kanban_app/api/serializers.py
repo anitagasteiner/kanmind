181b1f1a02e52178babba5993bfc9d4a77811ddd
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-17 15:15:48 +0100

    board serializer: tasks, members optimized

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -25,16 +25,20 @@ class BoardSerializer(serializers.ModelSerializer):
     #     source='tickets'
     # )
     # ticket_count = serializers.SerializerMethodField()
-
+    
+    # members = serializers.PrimaryKeyRelatedField(
+    #     queryset=Board.members.field.related_model.objects.all(), # User.objects
+    #     many=True
+    # )
     member_count = serializers.SerializerMethodField()
 
-    tasks = TaskSerializer(many=True, read_only=True)
-    task_ids = serializers.PrimaryKeyRelatedField(
-        queryset=Task.objects.all(),
-        many=True,
-        write_only=True,
-        source='tasks'
-    )
+    #tasks = TaskSerializer(many=True, read_only=True)
+    # task_ids = serializers.PrimaryKeyRelatedField(
+    #     queryset=Task.objects.all(),
+    #     many=True,
+    #     write_only=True,
+    #     source='tasks'
+    # )
     ticket_count = serializers.SerializerMethodField()
     tasks_to_do_count = serializers.SerializerMethodField()
     tasks_high_prio_count = serializers.SerializerMethodField()
@@ -42,19 +46,27 @@ class BoardSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Board
-        fields = ['id', 'title', 'member_count', 'tasks', 'task_ids', 'ticket_count', 'tasks_to_do_count','tasks_high_prio_count', 'owner_id']
+        fields = ['id', 'title', 'member_count', 'ticket_count', 'tasks_to_do_count','tasks_high_prio_count', 'owner_id']
         read_only_fields = ['id']
 
     def get_member_count(self, obj):
-        return obj.members.count() #TODO - Woher kommt "members"? Das gibt's doch gar nicht, oder?
+        return obj.members.count()
 
     def get_ticket_count(self, obj):
         return obj.tasks.count()
     
     def get_tasks_to_do_count(self, obj):
-        return obj.tasks.filter(status=Task.TO_DO).count()
+        return obj.tasks.filter(status='to_do').count()
     
     def get_tasks_high_prio_count(self, obj):
-        return obj.tasks.filter(priority=Task.HIGH).count()
+        return obj.tasks.filter(priority='high').count()
+    
+    def create(self, validated_data):
+        members = validated_data.pop('members', [])
+        owner = self.context['request'].user # Owner aus context (request.user)
+        board = Board.objects.create(owner=owner, **validated_data)
+        board.members.set(members)
+        return board
+
 
 
