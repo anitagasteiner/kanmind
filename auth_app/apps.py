37f8723fd662710af73d0529e47b4ce8a93535dc
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-11-28 10:38:16 +0100

    auth_app/apps.py Doku

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -1,6 +1,19 @@
+"""
+Django App configuration for the authentication app of Kanmind.
+
+This module defines the configuration class for the `auth_app` Django application.
+"""
+
 from django.apps import AppConfig
 
-
 class AuthAppConfig(AppConfig):
+    """
+    Configuration class for the authentication app.
+
+    Attributes:
+        default_auto_field (str): Default type for auto-generated primary keys.
+        name (str): Name of the Django application.
+    """
+        
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_app'
